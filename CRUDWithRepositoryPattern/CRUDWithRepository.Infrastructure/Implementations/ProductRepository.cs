737fa06c758887640b48684a241e81c666a093e0
kaushalpatelit1 <kaushalpatelit1@gmail.com>
2024-07-09 14:09:52 -0400

    CRUD project with Repository Pattern and Entity Framework

---
+++ CRUDWithRepositoryPattern/CRUDWithRepository.Infrastructure/Implementations/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using CRUDWithRepository.Core;
+using CRUDWithRepository.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDWithRepository.Infrastructure.Implementations
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly MyAppDbContext _context;
+
+        public ProductRepository(MyAppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Product>> GetAll()
+        {
+            var products = await _context.Products.ToListAsync();
+            return products;
+        }
+        public Task<Product> GetById(int productId)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task Add(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            Save();
+
+        }
+        public Task Update(Product product)
+        {
+            throw new NotImplementedException();
+            //Save();
+        }
+        public Task Delete(int productId)
+        {
+            throw new NotImplementedException();
+            //Save();
+        }
+        private void Save()
+        {
+            _context.SaveChanges(); 
+        }
+    }
+}
---
+++ CRUDWithRepositoryPattern/CRUDWithRepositoryPattern/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CRUDWithRepository.Infrastructure.Interfaces;
+using CRUDWithRepository.Core;
+
+namespace CRUDWithRepositoryPatternUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repository;
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;   
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _repository.GetAll();
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult CreateProduct()
+        { 
+            return View();  
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(Product model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await _repository.Add(model);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["errorMessage"] = "Invalid Product.";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = $"Invalid Product. Exception = {ex}";
+                throw;
+            }
+        }
+    }
+}
