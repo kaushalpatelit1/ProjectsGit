72a2d2d5cfa7d1aea54527df00df4f06009a90a9
kaushalpatelit1 <kaushalpatelit1@gmail.com>
2024-08-06 12:31:33 -0400

    Created Basic aspnetcore web API to get rooms in USA

---
+++ USAApi/USAApi/Controllers/RoomsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace USAApi.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRooms))]
+        public IActionResult GetRooms()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ USAApi/USAApi/Controllers/RootController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace USAApi.Controllers
+{
+    [Route("/")]
+    [ApiController] //this attribute let's asp.net core know that I am building a controller meant for an API. This adds features like automated model validation.
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRoot))] // this explicitly tells asp.net core that it should handle the GET Verb
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null),
+                rooms = new
+                {
+                    href = Url.Link(nameof(RoomsController.GetRooms), null)
+                }
+            };
+            return Ok(response);
+        }
+    }
+}
